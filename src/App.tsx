9b7edc6591015e09083dcd9249e027c1674fc411
Julia Peregudova <juliaperegudova@redcollar.ru>
2022-09-10 19:26:55 +0300

    git fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getUsers} from "./api/api";
+import {User} from "./types/user";
+import {Table} from "./components/Table/Table";
 
-function App() {
+const App = () => {
+
+    const [users, setUsers] = useState<User[]>([]);
+    const [usersToShow, setUsersToShow] = useState<User[]>([])
+
+    useEffect(() => {
+        getUsers().then((response) => {
+            setUsers(response.data)
+            console.log(response)
+        }).catch((e) => console.log(e));
+    }, [])
+
+    useEffect(() => {
+       setUsersToShow(users.slice(0, 10));
+    }, [users])
+
   return (
-    <div>
-      Hello!
-    </div>
+      <>
+          <Table users={usersToShow} />
+      </>
   );
 }
 
